96a907aafa7e0f9ee376482e0da00752770bfd5e
anmolsingh0001 <anmol.singh@kalvium.community>
2022-12-26 15:36:19 +0530

    imageCarousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -7,7 +7,59 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // implement the class below
 class Carousel extends Component {
-  
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         value:0,
+      }
+    }
+
+    random(){
+        let set=this.state.value
+        if(set>1){
+            set=0;
+        }
+        else{
+            set+=1;
+        }
+        this.setState({
+           
+            value:set,
+        })
+    }
+
+    back(){
+        let set=this.state.value
+        if(set<=0){
+            set=2;
+        }
+        else{
+            set-=1;
+        }
+        this.setState({
+           
+            value:set,
+        })
+    }
+
+  render (){
+    let data=images[this.state.value]
+    return(
+        <div>
+            <div className="click">
+            <ArrowBackIosIcon className="back" onClick={()=>{this.back()}}/>
+            </div>
+            <h1 className="style">{data.title}</h1>
+            <img className="img" src={data.img} alt='' />
+            <h1 className="style2">{data.subtitle}</h1>
+            <div className="click2">
+            <ArrowForwardIosIcon onClick={()=>this.random()} className='for' />
+            </div>
+        </div>
+       
+    )
+  }
 }
 
 export default Carousel;
